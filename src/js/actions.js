00a5a79d510a9e773aa657e2c5325f51914cae07
kyle <belfortk@gmail.com>
2017-11-01 10:24:49 -0700

    updated package.js & updated axios calls

--- src/js/actions.js
+++ src/js/actions.js
@@ -4,7 +4,7 @@ export function getMovieSearchResults(searchParam) {
 
   return {
     type: "GET_MOVIE_SEARCH_RESULTS",
-    payload: axios.get('http://www.omdbapi.com/?apikey=8730e0e&s=' + searchParam)
+    payload: axios.get('https://www.omdbapi.com/?apikey=8730e0e&s=' + searchParam)
     .then(function (response) {
       const { Search } = response.data;
       const results = [...Search];
@@ -16,7 +16,7 @@ export function getMovieSearchResults(searchParam) {
 export function getMovieDetails(id){
   return {
     type: 'GET_MOVIE_DETAILS',
-    payload: axios.get('http://www.omdbapi.com/?apikey=8730e0e&i=' + id)
+    payload: axios.get('https://www.omdbapi.com/?apikey=8730e0e&i=' + id)
     .then(response => response.data)
   };
 }
